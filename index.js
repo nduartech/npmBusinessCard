d22c90736e28ae8e40f7ac57dfe46a8a74f8a6cf
nduartech <nduartech101@gmail.com>
2024-06-05 22:35:31 -0400

    Contact

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-fs.readFile('./info.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
+console.log("{\n" +
+    "  \"name\": \"Nathan L. Duarte\",\n" +
+    "  \"website\": \"https://nduartech.github.io\",\n" +
+    "  \"email\": \"nduartech101@gmail.com\",\n" +
+    "  \"github\": \"https://github.com/nduartech\",\n" +
+    "  \"linkedin\": \"https://www.linkedin.com/in/nathanduarte\"\n" +
+    "}");
