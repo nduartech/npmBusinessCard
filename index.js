1b7d1a9a347f461307e8e09ed6c0a25206c192ea
nduartech <nduartech101@gmail.com>
2024-08-27 23:45:28 -0400

    Tweaks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 console.log("{\n" +
     "  \"name\": \"Nathan L. Duarte\",\n" +
-    "  \"website\": \"https://nduartech.github.io\",\n" +
+    "  \"website\": \"https://nathanld.xyz\",\n" +
     "  \"email\": \"nduartech101@gmail.com\",\n" +
     "  \"github\": \"https://github.com/nduartech\",\n" +
     "  \"linkedin\": \"https://www.linkedin.com/in/nathanduarte\"\n" +
